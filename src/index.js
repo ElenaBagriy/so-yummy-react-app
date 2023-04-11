8ecfe8eccccaa543ab7ac37391bda5fbbff36098
ElenaBagriy <elena.bagriy1989@gmail.com>
2023-04-11 10:14:14 +0200

    1

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter
-      basename="/so-yummy-react-app"
+      <BrowserRouter
+      // basename="/so-yummy-react-app"
     >
       <App />
       </BrowserRouter>
