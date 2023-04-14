7419269d6ba29f1180421a500e879d47afb366a7
Olena Denysenko <helenazinchenko@ukr.net>
2023-04-14 21:59:30 +0300

    styles finished + responsive without styles for validation scheme

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -3,13 +3,23 @@ import React from 'react';
 import { Formik, Field, Form, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 
-import picture from '../../images/welcomePage/img-sign-reg-desktop@2x.png';
-import { LinkStyled, RegisterStyled } from './RegisterFormStyled';
+import pictureDesktop from '../../images/welcomePage/img-sign-reg-desktop.png';
+import pictureDesktop2x from '../../images/welcomePage/img-sign-reg-desktop@2x.png';
+import pictureTabl from '../../images/welcomePage/img-sign-reg-tablet.png';
+import pictureTabl2x from '../../images/welcomePage/img-sign-reg-tablet@2x.png';
+import pictureMob from '../../images/welcomePage/img-sign-reg-mobile.png';
+import pictureMob2x from '../../images/welcomePage/img-sign-reg-mobile@2x.png';
+import pictureDesktopWebP from '../../images/welcomePage/img-sign-reg-desktop.webp';
+import pictureDesktopWebP2x from '../../images/welcomePage/img-sign-reg-desktop@2x.webp';
+import pictureTablWebP from '../../images/welcomePage/img-sign-reg-tablet.webp';
+import pictureTablWebP2x from '../../images/welcomePage/img-sign-reg-tablet@2x.webp';
+import pictureMobWebP from '../../images/welcomePage/img-sign-reg-mobile.webp';
+import pictureMobWebP2x from '../../images/welcomePage/img-sign-reg-mobile@2x.webp';
+import { FormWrapper, LinkStyled, RegisterStyled } from './RegisterFormStyled';
 import sprite from '../../images/welcomePage/symbol-defs.svg';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { loginUser, registerUser } from 'redux/user/userOperations';
-
 
 const RegisterSchema = Yup.object().shape({
   name: Yup.string()
@@ -31,58 +41,94 @@ export const RegisterForm = () => {
 
   return (
     <RegisterStyled>
-      <img src={picture} alt="" height="468" />
-      <Formik
-        initialValues={{
-          name: '',
-          email: '',
-          password: '',
-        }}
-        validationSchema={RegisterSchema}
-        onSubmit={(values, actions) => {
-          const { email, password } = values;
-          dispatch(registerUser(values))
-            .then(() => {dispatch(loginUser({email, password}))
-            // .unwrap()
-            .then(() => navigate('/main'))
-            .catch(() => navigate('/'));}
-            );
-          
-          // console.log(values);
-          // console.log(actions);
-          actions.resetForm();
-        }}
-      >
-        <Form>
-          <h2>Registration</h2>
-          <label>
-            <svg width="24" height="24">
-              <use href={sprite + '#icon-user'} />
-            </svg>
-            <Field name="name" placeholder="Name" />
-            <ErrorMessage className="error" name="name" component="div" />
-          </label>
+      <picture>
+        {/* desktop */}
+        <source
+          srcSet={`${pictureDesktop} 1x, ${pictureDesktop2x} 2x`}
+          media="(min-width: 1440px)"
+          type="image/png"
+        />
+        <source
+          srcSet={`${pictureDesktopWebP} 1x, ${pictureDesktopWebP2x} 2x`}
+          media="(min-width: 1440px)"
+          type="image/webp"
+        />
 
-          <label>
-            <svg width="24" height="24">
-              <use href={sprite + '#icon-mail'} />
-            </svg>
-            <Field name="email" placeholder="Email" />
-            <ErrorMessage className="error" name="email" component="div" />
-          </label>
-          <label>
-            <svg width="24" height="24">
-              <use href={sprite + '#icon-lock'} />
-            </svg>
-            <Field name="password" placeholder="Password" type="password" />
-            <ErrorMessage className="error" name="password" component="div" />
-          </label>
+        {/* tablet */}
+        <source
+          srcSet={`${pictureTabl} 1x, ${pictureTabl2x} 2x`}
+          media="(min-width: 768px)"
+          type="image/png"
+        />
+        <source
+          srcSet={`${pictureTablWebP} 1x, ${pictureTablWebP2x} 2x`}
+          media="(min-width: 768px)"
+          type="image/webp"
+        />
 
-          <button type="submit">Sign up</button>
-        </Form>
-      </Formik>
+        {/* mobile */}
+        <source
+          srcSet={`${pictureMob} 1x, ${pictureMob2x} 2x`}
+          media="(max-width: 767px)"
+          type="image/png"
+        />
+        <source
+          srcSet={`${pictureMobWebP} 1x, ${pictureMobWebP2x} 2x`}
+          media="(max-width: 767px)"
+          type="image/webp"
+        />
+        <img src={pictureMob} alt="girl sofa phone food" />
+      </picture>
+      <FormWrapper>
+        <Formik
+          initialValues={{
+            name: '',
+            email: '',
+            password: '',
+          }}
+          validationSchema={RegisterSchema}
+          onSubmit={(values, actions) => {
+            const { email, password } = values;
+            dispatch(registerUser(values)).then(() => {
+              dispatch(loginUser({ email, password }))
+                // .unwrap()
+                .then(() => navigate('/main'))
+                .catch(() => navigate('/'));
+            });
+            actions.resetForm();
+          }}
+        >
+          <Form>
+            <h2>Registration</h2>
+            <label>
+              <svg width="24" height="24">
+                <use href={sprite + '#icon-user'} />
+              </svg>
+              <Field name="name" placeholder="Name" />
+              <ErrorMessage className="error" name="name" component="div" />
+            </label>
 
-      <LinkStyled to="/signin">Sign in</LinkStyled>
+            <label>
+              <svg width="24" height="24">
+                <use href={sprite + '#icon-mail'} />
+              </svg>
+              <Field name="email" placeholder="Email" />
+              <ErrorMessage className="error" name="email" component="div" />
+            </label>
+            <label>
+              <svg width="24" height="24">
+                <use href={sprite + '#icon-lock'} />
+              </svg>
+              <Field name="password" placeholder="Password" type="password" />
+              <ErrorMessage className="error" name="password" component="div" />
+            </label>
+
+            <button type="submit">Register</button>
+          </Form>
+        </Formik>
+
+        <LinkStyled to="/signin">Sign in</LinkStyled>
+      </FormWrapper>
     </RegisterStyled>
   );
 };
--- src/components/RegisterForm/RegisterFormStyled.js
+++ src/components/RegisterForm/RegisterFormStyled.js
@@ -1,43 +1,134 @@
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-import back from '../../images/welcomePage/background-desktop@2x.png';
+import backMob from '../../images/welcomePage/background-mobile.png';
+import backMob2x from '../../images/welcomePage/background-mobile@2x.png';
+import backTabl from '../../images/welcomePage/background-tablet.png';
+import backTabl2x from '../../images/welcomePage/background-tablet@2x.png';
+import backDesktop from '../../images/welcomePage/background-desktop.png';
+import backDesktop2x from '../../images/welcomePage/background-desktop@2x.png';
 
 export const RegisterStyled = styled.div`
   position: relative;
-  padding: 122px 180px 163px 113px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 305px 20px 157px;
   background-color: ${props => props.theme.colors.white};
-  background-image: url('${back}');
-  background-position: left bottom;
+  background-image: url('${backMob}');
+  background-position: 50% 100%;
   background-repeat: no-repeat;
   background-size: contain;
 
+  //767px high-resolution
+  @media screen and (min-device-pixel-ratio: 2) and (max-width: 767px),
+    screen and (min-resolution: 192dpi) and (max-width: 767px),
+    screen and (min-resolution: 2dppx) and (max-width: 767px) {
+    background-image: url(${backMob2x});
+  }
+
+  //tablet
+  @media screen and (min-width: 768px) {
+    font-size: 28px;
+    padding: 444px 134px 100px;
+    background-image: url(${backTabl});
+  }
+
+  //768-1440px high-resolution
+  @media screen and (min-device-pixel-ratio: 2) and (min-width: 768px),
+    screen and (min-resolution: 192dpi) and (min-width: 768px),
+    screen and (min-resolution: 2dppx) and (min-width: 768px) {
+    background-image: url(${backTabl2x});
+  }
+
+  //desktop
+  @media screen and (min-width: 1440px) {
+    background-image: url(${backDesktop});
+    padding: 122px 180px 164px 760px;
+    justify-content: right;
+    align-items: end;
+  }
+
+  //1440px+ high-resolution
+  @media screen and (min-device-pixel-ratio: 2) and (min-width: 1440px),
+    screen and (min-resolution: 192dpi) and (min-width: 1440px),
+    screen and (min-resolution: 2dppx) and (min-width: 1440px) {
+    background-image: url(${backDesktop2x});
+  }
+
   img {
     position: absolute;
-    max-width: 532px;
-    top: 115px;
-    left: 113px;
+    top: 87px;
+    left: auto;
+    transform: translateX(-50%);
+    z-index: 1;
+
+    //tablet
+    @media screen and (min-width: 768px) {
+      top: 96px;
+      left: auto;
+      transform: translateX(-50%);
+    }
+
+    //desktop
+    @media screen and (min-width: 1440px) {
+      top: 115px;
+      left: 113px;
+      transform: translateX(0);
+    }
   }
 
   form {
     display: flex;
     flex-direction: column;
-    /* margin-left: 760px; */
     margin-left: auto;
-    padding: 44px 50px;
-    width: 500px;
-    /* height: 484px; */
+    margin-bottom: 18px;
+    padding: 32px 28px 40px;
+    width: 335px;
+    /* height: 419px; */
     border-radius: 30px;
     background-color: #2a2c36;
     color: ${props => props.theme.colors.white};
     box-shadow: 0px 4px 48px rgba(0, 0, 0, 0.1);
+    z-index: 2;
+
+    //tablet
+    @media screen and (min-width: 768px) {
+      width: 500px;
+      padding: 44px 50px;
+      font-size: 28px;
+    }
+
+    //desktop
+    @media screen and (min-width: 1440px) {
+    }
   }
 
-  label:not(:nth-child(4)) {
-    margin-bottom: 24px;
+  h2 {
+    font-size: 24px;
+    font-weight: 600;
+    line-height: 1.16;
+    margin-bottom: 18px;
+
+    //tablet
+    @media screen and (min-width: 768px) {
+      margin-bottom: 32px;
+      font-size: 28px;
+      line-height: 1.07;
+    }
   }
 
   label {
     position: relative;
+  }
+
+  label:not(:nth-child(4)) {
+    margin-bottom: 12px;
+
+    //tablet
+    @media screen and (min-width: 768px) {
+      margin-bottom: 24px;
+    }
   }
 
   svg {
@@ -48,37 +139,60 @@ export const RegisterStyled = styled.div`
   }
 
   input {
-    width: 400px;
-    height: 59px;
-    padding-top: 16px;
-    padding-left: 50px;
-    padding-bottom: 16px;
+    width: 279px;
+    height: 45px;
+    padding-top: 12px;
+    padding-left: 40px;
+    padding-bottom: 12px;
     border: 1px solid #fafafa;
     border-radius: 6px;
     background-color: transparent;
-    font-size: 18px;
-    line-height: 27px;
+    font-size: 14px;
+    line-height: 1.5;
     letter-spacing: -0.02em;
     color: ${props => props.theme.colors.white};
-  }
 
-  h2 {
-    font-size: 28px;
-    font-weight: 600;
-    margin-bottom: 32px;
+    //tablet
+    @media screen and (min-width: 768px) {
+      padding-top: 16px;
+      padding-left: 50px;
+      padding-bottom: 16px;
+      min-width: 400px;
+      height: 59px;
+      font-size: 18px;
+      line-height: 1.5;
+    }
+
+    //desktop
+    @media screen and (min-width: 1440px) {
+    }
   }
 
   button {
-    height: 59px;
-    margin-top: 50px;
-    background-color: ${props => props.theme.colors.green};
-    color: ${props => props.theme.colors.white};
+    width: 100%;
+    height: 45px;
+    margin-top: 28px;
+    font-size: 16px;
+    line-height: 1.12;
     border-radius: 6px;
     border: none;
+    background-color: ${props => props.theme.colors.green};
+    color: ${props => props.theme.colors.white};
 
     &:hover,
     &:focus {
       color: ${props => props.theme.colors.black};
+    }
+
+    //tablet
+    @media screen and (min-width: 768px) {
+      height: 56px;
+      margin-top: 50px;
+    }
+
+    //desktop
+    @media screen and (min-width: 1440px) {
+      height: 59px;
     }
   }
 
@@ -92,13 +206,28 @@ export const RegisterStyled = styled.div`
   }
 `;
 
+export const FormWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
 export const LinkStyled = styled(Link)`
-  display: block;
-  margin-top: 18px;
-  margin-left: 970px;
-  margin-right: auto;
   text-decoration: underline;
+  font-size: 14px;
+  line-height: 1.5;
   color: ${props => props.theme.colors.white};
+
+  //tablet
+  @media screen and (min-width: 768px) {
+    font-size: 16px;
+    line-height: 1.5;
+  }
+
+  //desktop
+  @media screen and (min-width: 1440px) {
+  }
 
   &:hover,
   &:focus {
