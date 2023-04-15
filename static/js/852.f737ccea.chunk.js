1ba9df1c2c1ef19ef1073937329485b6de25ae5d
ElenaBagriy <112576654+ElenaBagriy@users.noreply.github.com>
2023-04-15 21:26:06 +0000

    Deploying to gh-pages from @ ElenaBagriy/so-yummy-react-app@d2e1d335e49dbbb099659ced1bd66c20a20d2fcd 🚀

--- static/js/852.f737ccea.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkso_yummy_react_app=self.webpackChunkso_yummy_react_app||[]).push([[852],{852:function(e,r,a){a.r(r),a.d(r,{MainPage:function(){return t}});var n=a(184),t=function(){return(0,n.jsx)(n.Fragment,{children:"\u0421hooseYourBreakfast Search PreviewCategories"})}}}]);
-//# sourceMappingURL=852.f737ccea.chunk.js.map
