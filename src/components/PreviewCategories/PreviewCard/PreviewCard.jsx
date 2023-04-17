2d388dafc6abfef2e76df0d4d8a4aace49368a52
ElenaBagriy <elena.bagriy1989@gmail.com>
2023-04-18 01:39:10 +0200

    1

--- src/components/PreviewCategories/PreviewCard/PreviewCard.jsx
+++ src/components/PreviewCategories/PreviewCard/PreviewCard.jsx
@@ -1,8 +1,8 @@
-import { useState } from "react";
+// import { useState } from "react";
 import { onCapitalise } from "services/api/onCapitalise";
 
 export const PreviewCard = ({ category }) => {
-    const [recipes, setRecipes] = useState([]);
+    // const [recipes, setRecipes] = useState([]);
 
 console.log(category);
     return <div>
--- src/components/PreviewCategories/PreviewCategories.jsx
+++ src/components/PreviewCategories/PreviewCategories.jsx
@@ -1,8 +1,8 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { Container } from "reusableComponents/Container/Container";
-import { PreviewCard } from "./PreviewCard/PreviewCard";
+// import { PreviewCard } from "./PreviewCard/PreviewCard";
 import { onCapitalise } from "services/api/onCapitalise";
-import { ItemCard } from "reusableComponents/ItemCard/ItemCard";
+// import { ItemCard } from "reusableComponents/ItemCard/ItemCard";
 import { StyledTitle } from "reusableComponents/ManePageTitle/ManePageTitle.styled";
 import { useDispatch, useSelector } from "react-redux";
 import { selectMainPageRecipes } from "redux/selectors";
@@ -22,9 +22,9 @@ export const PreviewCategories = () => {
     const dispatch = useDispatch();
     const mainPageRecipes = useSelector(selectMainPageRecipes);
 
-    const [popularCategories, setPopularCategories] = useState([]);
-    const [popularRecipes, setPopularRecipes] = useState([]);
-    const [isLoading, setIsLoading] = useState(false);
+    // const [popularCategories, setPopularCategories] = useState([]);
+    // const [popularRecipes, setPopularRecipes] = useState([]);
+    // const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
         dispatch(getRecipesMainPage());
