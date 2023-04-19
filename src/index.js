aa6c3bbac464f7c2408252c4e7db873ba7ac49b8
ElenaBagriy <elena.bagriy1989@gmail.com>
2023-04-19 12:37:15 +0200

    update

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter
-        basename="/so-yummy-react-app"
+        // basename="/so-yummy-react-app"
         >
           <App />
         </BrowserRouter>
